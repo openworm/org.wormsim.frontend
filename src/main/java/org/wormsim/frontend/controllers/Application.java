7f8f439e0ef9714dc5fbea44a45b636248f8e65b
Matt Olson <mattlolson@gmail.com>
2014-10-06 16:25:20 -0700

    Add endpoints for setting worm info

--- src/main/java/org/wormsim/frontend/controllers/Application.java
+++ src/main/java/org/wormsim/frontend/controllers/Application.java
@@ -1,13 +1,18 @@
 package org.wormsim.frontend.controllers;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.osgi.framework.BundleContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.wormsim.frontend.models.User;
+import org.wormsim.frontend.models.WormInfo;
+import org.wormsim.frontend.stormpath.UserFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,18 +23,34 @@ public class Application {
     @Autowired
     BundleContext bundleContext;
 
-    public Application() {}
+    public Application() {
+    }
 
-    @RequestMapping(value="/", method=RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView home() {
-        Map<String, Subject> modelMap = new HashMap<>();
-        modelMap.put("subject", SecurityUtils.getSubject());
+        Map<String, User> modelMap = new HashMap<>();
+        modelMap.put("user", UserFactory.current());
         return new ModelAndView("home", modelMap);
     }
 
-    @RequestMapping(value="/create", method=RequestMethod.GET)
-    public String create() {
-        return "createworm";
+    @RequestMapping(value = "/wormInfo", method = RequestMethod.GET)
+    @RequiresAuthentication
+    public String setWormInfo() {
+        return "setWormInfo";
     }
+
+    @RequestMapping(value = "/wormInfo", method = RequestMethod.POST)
+    @ResponseBody
+    @RequiresAuthentication
+    public ModelAndView doSetWormInfo(@ModelAttribute WormInfo wormInfo, BindingResult bindingResult) {
+        User currentUser = UserFactory.current();
+        currentUser.setWormName(wormInfo.getWormName());
+        currentUser.setWormColor(wormInfo.getWormColor());
+        currentUser.save();
+
+        //TODO: redirect to org.geppetto.frontend if worminfo is set successfully (use bundleContext)
+        return new ModelAndView("redirect:/");
+    }
+
 
 }
