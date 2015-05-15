43dd127cc184171dd8c7ca799a7eea55c35f17cf
Mihai Raulea <mihairaulea@gmail.com>
2015-05-15 15:35:40 +0300

    Landing email merge

--- src/main/java/org/wormsim/frontend/controllers/Application.java
+++ src/main/java/org/wormsim/frontend/controllers/Application.java
@@ -24,29 +24,62 @@ import org.wormsim.frontend.stormpath.UserFactory;
 @Controller
 public class Application {
 
-	@Autowired
-	BundleContext bundleContext;
+    @Autowired
+    BundleContext bundleContext;
 
-	public Application() {
-	}
+    public Application() {
+    }
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home() {
-    	// force going through worminfo each time
-		User user = UserFactory.current();
-        if(user!=null) user.setWormName(null);
-        return setWormInfo();
-        /*
-			return new ModelAndView("redirect:/wormInfo");
-		} else if (user != null) {
-			return new ModelAndView("userhome", getUserMap());
-		} else {
-			return new ModelAndView("home", getUserMap());
-		}
-		*/
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home() {
 
-	@RequestMapping(value = "/ajaxSetLandingPageEmail", method = RequestMethod.POST)
+        User user = UserFactory.current();
+        if(user != null && (user.getWormName() == null || user.getWormName().isEmpty())) {
+            return new ModelAndView("redirect:/wormInfo");
+        } else if(user != null) {
+            return new ModelAndView("userhome", getUserMap());
+        } else {
+            return new ModelAndView("home", getUserMap());
+        }
+
+    }
+
+    @RequestMapping(value = "/wormInfo", method = RequestMethod.GET)
+    public ModelAndView setWormInfo() {
+        return new ModelAndView("setWormInfo", getUserMap());
+    }
+
+    @RequestMapping(value = "/wormInfo", method = RequestMethod.POST)
+    @ResponseBody
+    public ModelAndView doSetWormInfo(@ModelAttribute WormInfo wormInfo, BindingResult bindingResult) {
+        User currentUser = UserFactory.current();
+        currentUser.setWormName(wormInfo.getWormName());
+        currentUser.setWormColor(wormInfo.getWormColor());
+        currentUser.save();
+        UserFactory.setSessionUser(currentUser);
+
+        //TODO: redirect to org.geppetto.frontend if worminfo is set successfully (use bundleContext)
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(value = "/setWormInfo", method = RequestMethod.POST)
+    @ResponseBody
+    public void ajaxSetWormInfo(@RequestBody WormInfo wormInfo) {
+        User currentUser = UserFactory.current();
+
+        String name = wormInfo.getWormName(), color = wormInfo.getWormColor();
+        if(name != null && !name.isEmpty()) {
+            currentUser.setWormName(name);
+        }
+        if(color != null && !color.isEmpty()) {
+            currentUser.setWormColor(color);
+        }
+
+        currentUser.save();
+        UserFactory.setSessionUser(currentUser);
+    }
+
+    @RequestMapping(value = "/ajaxSetLandingPageEmail", method = RequestMethod.POST)
 	public void ajaxSetLandingPageEmail(HttpServletRequest req,
 			HttpServletResponse res) {
 		boolean callSucceeded = true;
@@ -66,54 +99,17 @@ public class Application {
 
 		}
 	}
+    
+    @RequestMapping(value = "/simulator", method = RequestMethod.GET)
+    public void simulator(HttpServletResponse response) {
+        response.setStatus(302);
+        response.setHeader("Location", "/org.geppetto.frontend");
+    }
 
-	@RequestMapping(value = "/wormInfo", method = RequestMethod.GET)
-	public ModelAndView setWormInfo() {
-		return new ModelAndView("setWormInfo", getUserMap());
-	}
-
-	@RequestMapping(value = "/wormInfo", method = RequestMethod.POST)
-	@ResponseBody
-	public ModelAndView doSetWormInfo(@ModelAttribute WormInfo wormInfo,
-			BindingResult bindingResult) {
-		User currentUser = UserFactory.current();
-		currentUser.setWormName(wormInfo.getWormName());
-		currentUser.setWormColor(wormInfo.getWormColor());
-		currentUser.save();
-		UserFactory.setSessionUser(currentUser);
-
-		// TODO: redirect to org.geppetto.frontend if worminfo is set
-		// successfully (use bundleContext)
-		return new ModelAndView("redirect:/");
-	}
-
-	@RequestMapping(value = "/setWormInfo", method = RequestMethod.POST)
-	@ResponseBody
-	public void ajaxSetWormInfo(@RequestBody WormInfo wormInfo) {
-		User currentUser = UserFactory.current();
-
-		String name = wormInfo.getWormName(), color = wormInfo.getWormColor();
-		if (name != null && !name.isEmpty()) {
-			currentUser.setWormName(name);
-		}
-		if (color != null && !color.isEmpty()) {
-			currentUser.setWormColor(color);
-		}
-
-		currentUser.save();
-		UserFactory.setSessionUser(currentUser);
-	}
-
-	@RequestMapping(value = "/simulator", method = RequestMethod.GET)
-	public void simulator(HttpServletResponse response) {
-		response.setStatus(302);
-		response.setHeader("Location", "/org.geppetto.frontend");
-	}
-
-	private Map<String, User> getUserMap() {
-		Map<String, User> modelMap = new HashMap<>();
-		modelMap.put("user", UserFactory.current());
-		return modelMap;
-	}
+    private Map<String, User> getUserMap() {
+        Map<String, User> modelMap = new HashMap<>();
+        modelMap.put("user", UserFactory.current());
+        return modelMap;
+    }
 
 }
