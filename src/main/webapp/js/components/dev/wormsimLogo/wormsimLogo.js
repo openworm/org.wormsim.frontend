d393da91d553ef13bbd54b616be35f4b9546ebf0
johnidol <giovanni.idili@gmail.com>
2015-07-08 16:00:41 +0100

    update logo control

--- src/main/webapp/js/components/dev/wormsimLogo/wormsimLogo.js
+++ src/main/webapp/js/components/dev/wormsimLogo/wormsimLogo.js
@@ -3,7 +3,7 @@ define(function(require) {
     var link = document.createElement("link");
     link.type = "text/css";
     link.rel = "stylesheet";
-    link.href = "js/components/dev/wormsimLogo/wormsim-logo.css";
+    link.href = "assets/js/components/dev/wormsimLogo/wormsim-logo.css";
     document.getElementsByTagName("head")[0].appendChild(link);
 
 });
