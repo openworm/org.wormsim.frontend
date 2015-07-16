c93e0f56f4d93d78ffbebd58749b358717511882
johnidol <giovanni.idili@gmail.com>
2015-07-16 11:56:38 +0100

    added config for button bar widget to toggle cuticle on/off

---
+++ src/main/webapp/js/components/dev/wormsimbuttonbar/buttonBarConfig.js
@@ -0,0 +1,22 @@
+{
+    "Cuticle / Muscles toggle": {
+        "toggle": {
+            "cuticleBtn": {
+                "actions": [
+                    "wormsim.show(false)"
+                ],
+                "icon": "gpt-worm",
+                "label": "Cuticle",
+                "tooltip": "Show cuticle"
+            },
+            "musclesBtn": {
+                "actions": [
+                    "wormsim.hide(false)"
+                ],
+                "icon": "gpt-ion-channel",
+                "label": "Muscles",
+                "tooltip": "Show muscles"
+            }
+        }
+    }
+}
