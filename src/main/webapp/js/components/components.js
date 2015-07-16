7dc7caf58139822c5121821702e09a51f50c98df
johnidol <giovanni.idili@gmail.com>
2015-07-16 14:02:32 +0100

    added the geppetto components file to swap in for wormsim

---
+++ src/main/webapp/js/components/components.js
@@ -0,0 +1,6 @@
+define(function(require) {
+require('jsx!./dev/customrenderer/AnimationCustomRenderer');
+require('jsx!./dev/wormsimlogo/wormsimLogo');
+require('jsx!./dev/wormsimsimulationcontrols/ExperimentControls');
+require('jsx!./dev/cameracontrols/CameraControls');
+});
