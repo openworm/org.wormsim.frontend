fc660b23c80fce7bf6f9811ebe4bff53b02715c5
johnidol <giovanni.idili@gmail.com>
2015-07-22 13:26:04 +0100

    deployment scripts

---
+++ utilities/deployWormSimData.py
@@ -0,0 +1,36 @@
+import os
+import os.path
+import shutil
+import stat
+
+def copytree(src, dst, symlinks = False, ignore = None):
+  if not os.path.exists(dst):
+    os.makedirs(dst)
+    shutil.copystat(src, dst)
+  lst = os.listdir(src)
+  if ignore:
+    excl = ignore(src, lst)
+    lst = [x for x in lst if x not in excl]
+  for item in lst:
+    s = os.path.join(src, item)
+    d = os.path.join(dst, item)
+    if symlinks and os.path.islink(s):
+      if os.path.lexists(d):
+        os.remove(d)
+      os.symlink(os.readlink(s), d)
+      try:
+        st = os.lstat(s)
+        mode = stat.S_IMODE(st.st_mode)
+        os.lchmod(d, mode)
+      except:
+        pass # lchmod not available
+    elif os.path.isdir(s):
+      copytree(s, d, symlinks, ignore)
+    else:
+      shutil.copy2(s, d)
+
+git_path = '/Users/[USERNAME]/git/openworm'
+source_dir = git_path + '/org.wormsim.frontend/appdata'
+dest_dir = '/usr/local/Cellar/virgo/virgo-tomcat-server-3.6.2.RELEASE/appdata'
+
+copytree(source_dir, dest_dir)
