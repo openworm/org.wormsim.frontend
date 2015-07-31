680aafca7eea3d7b27afdbdc4f8b75081a2f40ba
johnidol <giovanni.idili@gmail.com>
2015-07-31 15:09:18 +0100

    script now runs on both linux and macos without modifications

--- utilities/applyWormSimTheme.py
+++ utilities/applyWormSimTheme.py
@@ -1,7 +1,7 @@
 import os
 
-git_path = '../..'
+git_path = '../../org.geppetto.frontend/src/main/webapp/'
 
-os.system("find " + git_path + "/org.geppetto.frontend/src/main/webapp/ -type f \( -name '*.css' -or -name '*.less' \) -exec sed -i '' 's/#fc6320/#D6F729/g' {} \;")
-os.system("find " + git_path + "/org.geppetto.frontend/src/main/webapp/ -type f \( -name '*.css' -or -name '*.less' \) -exec sed -i '' 's/#fd6808/#D6F729/g' {} \;")
-os.system("find " + git_path + "/org.geppetto.frontend/src/main/webapp/less/main.less -type f \( -name '*.js' -or -name '*.css' -or -name '*.less' \) -exec sed -i '' 's/background-color: #000000/background-color: #222222/g' {} \;")
+os.system("find {0} -type f \( -name '*.css' -or -name '*.less' \) | xargs sed -i -e 's/#fc6320/#D6F729/g' ".format(git_path))
+os.system("find {0} -type f \( -name '*.css' -or -name '*.less' \) | xargs sed -i -e 's/#fd6808/#D6F729/g' ".format(git_path))
+os.system("sed -i -e 's/background-color: #000000/background-color: #222222/g' {0}less/main.less ".format(git_path))
