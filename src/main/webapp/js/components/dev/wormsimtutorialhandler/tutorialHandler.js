2473f60f56c91a3f9de44022946f29ca0f9864c8
Stephen Larson <stephen.larson@gmail.com>
2015-08-08 17:14:06 -0700

    tweaks to pacing, messaging

--- src/main/webapp/js/components/dev/wormsimtutorialhandler/tutorialHandler.js
+++ src/main/webapp/js/components/dev/wormsimtutorialhandler/tutorialHandler.js
@@ -14,13 +14,13 @@ define(function(require) {
     sphTutorialMessages.push("Other than straight runs, I can do fancier things like engage in turns, reverses and other behaviours.  If you are interested in the behaviour of my fellow worm friends, check out <a href='https://www.youtube.com/user/wormbehavior' target='_blank'>these videos</a> by the <a href='http://www.google.com/url?q=http%3A%2F%2Fwormbehavior.mrc-lmb.cam.ac.uk%2F&sa=D&sntz=1&usg=AFQjCNGYmP8hCUFsrU0ofv_6SGv1Cm-p7A' target='_blank'>C. elegans behavioural database.</a>");
     sphTutorialActions.push('');
 
-    sphTutorialMessages.push("Even as simple as the forward locomotion looks though, there are still a great number of open questions about how my brain generates even this movement.");
+    sphTutorialMessages.push("Even as simple as the forward locomotion looks though, there are still a great number of open questions about how my brain generates even this movement.  Scientists trying to figure me out sure have a bunch of ideas for how my cells do this!  But they still haven't completely figured me out!  Isn't that strange?");
     sphTutorialActions.push('');
 
-    sphTutorialMessages.push("Scientists trying to figure me out sure have a bunch of ideas for how my cells do this!  But they still haven't completely figured me out!  Isn't that strange?  The OpenWorm community talked about this in <a href='https://www.youtube.com/watch?v=puB8R9PW3BI&index=10&list=PL8ACJC0fGE7D-EkkR7EFgQESpHONC_kcI' target='_blank'>one of our OpenWorm journal clubs.</a>");
+    sphTutorialMessages.push("The OpenWorm community talked about this mystery in <a href='https://www.youtube.com/watch?v=puB8R9PW3BI&index=10&list=PL8ACJC0fGE7D-EkkR7EFgQESpHONC_kcI' target='_blank'>one of our OpenWorm journal clubs.</a>  Part of the mystery deals with my muscles.");
     sphTutorialActions.push('');
 
-    sphTutorialMessages.push("Part of the mystery deals with my muscles.  Under my skin, I have 95 body wall muscles along my body, which are the primary drivers of my forward locomotion.  The current model that you can see has my correct anatomy.  Feel free to use your mouse to rotate me around!");
+    sphTutorialMessages.push("Under my skin, I have 95 muscles along my body, which are the primary drivers of my forward locomotion.  The current model that you can see has my correct anatomy.  Feel free to use your mouse to rotate me around!");
     sphTutorialActions.push('wormsim.changeOpacity(0.3,false);wormsim.show(true);');
 
     sphTutorialMessages.push("The motion of each muscle you see here is the result of a complex physics simulation we call Sibernetic.  My friends at OpenWorm worked really hard on it!  You can  <a href='http://www.sibernetic.org' target='_blank'>learn more about Sibernetic online</a>.");
@@ -35,13 +35,13 @@ define(function(require) {
     sphTutorialMessages.push("Can you rotate me around so you can find the one yellow highlighted muscle that's generating this graph?  Feel free to zoom up close to see it.  That's the one I call 'VR12' :) ");
     sphTutorialActions.push('');
 
-    sphTutorialMessages.push("Check it out; you can freeze me in place!  See those buttons in the upper right corner?  You can use them to get me moving again and freeze me whenever you like.");
+    sphTutorialMessages.push("Check it out; you can FREEZE me IN PLACE!  See those play / pause / stop buttons in the upper right corner?  You can use them to get me moving again and freeze me whenever you like.");
     sphTutorialActions.push('Project.getActiveExperiment().pause();');
 
-    sphTutorialMessages.push("One of the next major steps for the OpenWorm project is to incorporate more data about the muscular activity of worm.  We are currently in the process of setting up collaborations with laboratories around the world to fill this gap.");
+    sphTutorialMessages.push("One of the next major steps for the OpenWorm project is to incorporate more data about the muscular activity of worm.  We've got a <a href='https://github.com/openworm/muscle_model/blob/addb61f370bab4e510a7610691524df172f03eba/README.md#introduction' target='_blank'>whole project</a> dedicated to building on what's been done to have a great model of each of these muscles that is as accurate as possible.");
     sphTutorialActions.push('Project.getActiveExperiment().play({step:2});');
 
-    sphTutorialMessages.push("OpenWorm wants to further understanding by providing an integrated simulation where both the worm’s biology and the physics of the surrounding environment is simulated.");
+    sphTutorialMessages.push("After you are done checking out my SWEET muscles (ha ha) you should go check out my brain cells over at the neuron simulation.  See you over there!");
     sphTutorialActions.push('');
 
 //  sphTutorialActions.push('Project.getActiveExperiment().pause();');
