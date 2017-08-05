e487ed3cd7fc73f0f78f1c3020d615fb785b5c06
Stephen Larson <stephen.larson@gmail.com>
2017-08-05 11:47:14 -0400

    First cut of docker files -- not yet tested

---
+++ docker/components.js
@@ -0,0 +1,8 @@
+define(function(require) {
+require('jsx!./dev/wormsimSimulationcontrols/SimulationControls');
+require('jsx!./dev/cameracontrols/CameraControls');
+require('jsx!./dev/tutorial/IntroModal');
+require('jsx!./dev/wormsimlogo/wormsimLogo');
+require('jsx!./dev/share/share');
+require('jsx!./dev/contact/contact');
+});
